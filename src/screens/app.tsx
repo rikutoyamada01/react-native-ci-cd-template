ad28c5b071266c0172fe1c82d33665ff44ac231b
ymd <ymd20060720@gmail.com>
2025-04-08 23:30:48 +0200

    first init

---
+++ src/screens/app.tsx
@@ -0,0 +1,173 @@
+import { useRef, useState, type FC } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  type ImageSourcePropType,
+} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
+import { registerRootComponent } from "expo";
+import * as ImagePicker from "expo-image-picker";
+import * as Medialibrary from "expo-media-library";
+import { StatusBar } from "expo-status-bar";
+import DomToImage from "dom-to-image";
+
+import PlaceholderImage from "@/assets/images/background-image.png";
+import { Button } from "@/components/button";
+import { CircleButton } from "@/components/circle-button";
+import { EmojiList } from "@/components/emoji-list";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { EmojiSticker } from "@/components/emoji-sticker";
+import { IconButton } from "@/components/icon-button";
+import { ImageViewer } from "@/components/image-viewer";
+
+const App: FC = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | null>(
+    null,
+  );
+  const [status, requestPermission] = Medialibrary.usePermissions();
+  const imageRef = useRef(null);
+
+  if (status === null) {
+    void requestPermission();
+  }
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== "web") {
+      if (imageRef.current) {
+        try {
+          const localUri = await captureRef(imageRef, {
+            height: 400,
+            quality: 1,
+          });
+
+          await Medialibrary.saveToLibraryAsync(localUri);
+          if (localUri) {
+            alert("画像を保存しました。");
+          }
+        } catch (error) {
+          console.error("Error saving image:", error);
+          alert("画像の保存に失敗しました。");
+        }
+      }
+    } else {
+      if (imageRef.current) {
+        try {
+          const dataUrl = await DomToImage.toJpeg(imageRef.current, {
+            quality: 0.95,
+            width: 320,
+            height: 440,
+          });
+
+          const link = document.createElement("a");
+          link.download = "sticker-smash.jpeg";
+          link.href = dataUrl;
+          link.click();
+        } catch (error) {
+          console.error("Error saving image:", error);
+        }
+      }
+    }
+  };
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("画像が選択されませんでした。");
+    }
+  };
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage as ImageSourcePropType}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
+        </View>
+      </View>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="リセット" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="保存"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="写真を選択"
+            onPress={pickImageAsync}
+          ></Button>
+          <Button
+            label="この写真を使用"
+            onPress={() => setShowAppOptions(true)}
+          ></Button>
+        </View>
+      )}
+      <StatusBar style="light" />
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+
+registerRootComponent(App);
